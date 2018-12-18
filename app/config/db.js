a0ff92e2de15f6bebfe10d44ba2cb301f0fd9ecc
abaykerimov <abay-001@mail.ru>
2018-12-19 02:43:54 +0600

    updated

--- app/config/db.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Абай on 18.12.2018.
- */
-module.exports = {
-    url : 'mongodb://direwolf:aba3899300@ds247838.mlab.com:47838/dev'
-};
--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,9 +1,54 @@
 /**
  * Created by Абай on 18.12.2018.
  */
+const ObjectId = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectId(id) };
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send(item);
+            }
+        });
+    });
+    
     app.post('/notes', (req, res) => {
-        // Здесь будем создавать заметку.
-        res.send('Hello')
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+    });
+
+    app.put ('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectId(id) };
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').update(details, note, (err, result) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send(note);
+            }
+        });
+    });
+
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectId(id) };
+        db.collection('notes').remove(details, (err, item) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send('Note ' + id + ' deleted!');
+            }
+        });
     });
 };
